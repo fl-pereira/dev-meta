831d072e1332f737bce09de725a4021c7fa0566a
Felipe Pereira <fpereira.media@gmail.com>
2022-07-14 11:41:31 -0300

    correcao heroku again

--- backend/src/main/java/com/devsuperior/devmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/devmeta/services/SmsService.java
@@ -33,12 +33,14 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		String amount = String.format("%.0f", sale.getAmount());
+		
 		String msg = 	"O vendedor "
 						+ sale.getSellerName()
 						+ " foi destaque em "
 						+ date
 						+ " com um total de R$ "
-						+ String.format("%.0f", sale.getAmount());
+						+ amount;
 		
 		Twilio.init(twilioSid, twilioKey);
 
