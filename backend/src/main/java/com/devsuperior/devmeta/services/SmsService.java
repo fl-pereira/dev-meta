634f652a1c5c853b0ca9cf1000577fc28100c9e7
Felipe Pereira <fpereira.media@gmail.com>
2022-07-15 23:27:07 -0300

    sms fix

--- backend/src/main/java/com/devsuperior/devmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/devmeta/services/SmsService.java
@@ -34,7 +34,8 @@ public class SmsService {
 
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 			
-		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date + " com o total de R$ " + String.format("%.2f",sale.getAmount()).replace(".", ",");
+		String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
+				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
 
